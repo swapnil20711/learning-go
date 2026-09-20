928ee87bb7aa73d85f5e83ee6b47e1940d446657
swapnil20711 <swapnil.bhojwani@gmail.com>
2026-09-20 23:49:45 +0530

    init go repo

---
+++ variables/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Swapnil"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type : %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type : %T \n", isLoggedIn)
+
+	var smallVal int = 256
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type : %T \n", smallVal)
+
+	var smallFloat float64 = 255.455431231211331
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type : %T \n", smallFloat)
+
+	//default value and some aliases
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type : %T \n", anotherVariable)
+
+	//implicit type
+	var website = "swapnilbhojwani.com"
+	fmt.Println(website)
+	fmt.Printf("Variable is of type : %T \n", website)
+
+}
