c89f3dfbb40e86ccb0c900760069f3c50ccd9861
swapnil20711 <swapnil.bhojwani@gmail.com>
2026-09-22 00:04:29 +0530

    feat: implement a map to be returne

--- hello/hello.go
+++ hello/hello.go
@@ -13,12 +13,12 @@ func main() {
 	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("Swapnil")
-
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		// If an error was returned, print it to the console and
 		// exit the program.
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
