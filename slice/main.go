ebf81b311b4b17ce78ef5705acfe6597552092ef
swapnil20711 <swapnil.bhojwani@gmail.com>
2026-09-27 01:24:21 +0530

    feat: learnt about slices in go

---
+++ slice/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// numbers := []int{1, 2, 3}
+
+	// numbers = append(numbers, 3, 10, 12, 13, 14, 15, 16)
+
+	// fmt.Println("Numbers :", numbers)
+	// fmt.Printf("Number has data type %T\n", numbers[0])
+
+	// fmt.Println("Length of numbers is :", len(numbers))
+
+	// names := []string{}
+
+	// fmt.Println("names: ", names)
+
+	numbers := make([]int, 3, 5)
+
+	numbers = append(numbers, 4)
+	numbers = append(numbers, 98)
+	numbers = append(numbers, 6)
+
+	fmt.Println("Slice :", numbers)
+	fmt.Println("Length :", len(numbers))
+	fmt.Println("Capacity :", cap(numbers))
+
+	stock := make([]int, 0)
+
+	fmt.Println("Slice :", stock)
+	fmt.Println("Length :", len(stock))
+	fmt.Println("Capacity :", cap(stock))
+}
